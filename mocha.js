c76c4bde0cedd48a94a9b676877b1e35fdc0d907
Daniel Fields <daniel.s.fields@gmail.com>
2017-06-08 16:51:47 -0400

    Adding a module to that creates additional overrides for mocha.

---
+++ mocha.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    mocha: true,
+  },
+  rules: {
+    'func-names': 0,
+    'prefer-arrow-callback': 0,
+    'space-before-function-paren': 0,
+    'global-require': 0,
+    'no-unused-expressions': 0,
+  },
+};
